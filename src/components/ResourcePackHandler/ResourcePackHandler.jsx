de8e076b5115c7dea079382dd4633a2956aac00e
Nuno Oliveira <nuno.magoliveira@gmail.com>
2021-11-19 20:54:45 +0100

    Home folder wip

--- src/components/ResourcePackHandler/ResourcePackHandler.jsx
+++ src/components/ResourcePackHandler/ResourcePackHandler.jsx
@@ -1,6 +1,17 @@
+import { useEffect, useState } from 'react';
 import styles from './ResourcePackHandler.module.css';
+import fs from 'fs'
+import os from 'os'
 
 const ResourcePackHandler = () => {
+  const [localVersion, setLocalVersion] = useState('0')
+
+  useEffect(() => {
+    console.log('os', os.homedir())
+    console.log('process.env', process.env.USERPROFILE)
+    // fs.readFile(process.env.APPDATA)
+  }, [])
+
   return (
     <div className={styles.resourcePack}>
       <p>Resource pack status</p>
